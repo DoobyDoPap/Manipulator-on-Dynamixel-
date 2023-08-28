903bbc876369f30cca639bcc0df9d4525cb6df88
Isha <142431248+IshaNair09@users.noreply.github.com>
2023-08-28 22:30:28 +0530

    Update LED BLINK

--- ledblinktask.c
+++ ledblinktask.c
@@ -1,71 +1,66 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include <driver/gpio.h>
-#include "esp_log.h"
-#define BG_LED_1 12
-#define BG_LED_2 33
-#define BG_LED_3 25
-#define BG_LED_4 26
-#define BG_LED_5 27
-#define BG_LED_6 14
-#define BG_LED_7 13
-#define BG_LED_8 23
-void set_led(uint8_t data,const int *pin_out)
-{
-for(int i=0;i<8;i++)
-{
-if ((data & 0x80) == 0x80)
-            {
-                // set gpio value to 1
-                gpio_set_level((gpio_num_t)pin_out[i], 1);
-            }
-            else
-            {
-                // set gpio value to 0
-                gpio_set_level((gpio_num_t)pin_out[i], 0);
-            }
-            data= data<<1;
-            }
-            
-            }
-void app_main(){
-static const int pin_out[8] = {BG_LED_1,BG_LED_2,BG_LED_4,BG_LED_4,BG_LED_5,BG_LED_6,BG_LED_7,BG_LED_8};
-uint8_t data;
-static const char *TAG = "blink_code";
-gpio_config_t io_conf;
-io_conf.intr_type = GPIO_INTR_DISABLE;
-io_conf.mode = GPIO_MODE_OUTPUT;
-io_conf.pin_bit_mask = 0;
-for (int i = 0; i < 8; i++) {
-        io_conf.pin_bit_mask |= (1ULL << led_pins[i]);
-    }
-io_conf.mode = GPIO_MODE_OUTPUT;
-io_conf.pull_up_en =0;
-io_conf.pull_down_en = 1;
+#include<stdio.h> 
+#include"driver/gpio.h"  
+#include "freertos/FreeRTOS.h"  
+ #include "freertos/task.h"  
+ 
 
-esp_err_t err = gpio_config(&io_conf);
-if(err == ESP_OK){
-for(int a=0;a<5;a++){
-set_led(0xFF,pin_out);
-vTaskDelay(1000 / portTICK_RATE_MS);
-set_led(0x00,pin_out);
-vTaskDelay(1000 / portTICK_RATE_MS);
-}
-data = 0x01;
-while(1)
-{
-if(data == 0x00){
-data = 0x01;
-}
-set_led(data,pin_out);
-data=data<<1;
-vTaskDelay(1000 / portTICK_RATE_MS);
-}
-}
-  else
-    {
-        ESP_LOGE(TAG, "error: %s", esp_err_to_name(err));
-        return;
-      }
-      }
+static const gpio_num_t step_pin_m1 = GPIO_NUM_12;
+static const gpio_num_t step_pin_m2 = GPIO_NUM_33;
+static const gpio_num_t step_pin_m3 = GPIO_NUM_25;
+static const gpio_num_t step_pin_m4 = GPIO_NUM_26;
+static const gpio_num_t step_pin_m5 = GPIO_NUM_27;
+static const gpio_num_t step_pin_m6 = GPIO_NUM_14;
+static const gpio_num_t step_pin_m7 = GPIO_NUM_13;
+static const gpio_num_t step_pin_m8 = GPIO_NUM_23; 
+static const  gpio_mode_t led_mode = GPIO_MODE_OUTPUT; 
+ 
+  
+ void app_main(){    
+         
+        gpio_set_direction( step_pin_m1, led_mode);  
+        gpio_set_direction( step_pin_m2, led_mode );   
+        gpio_set_direction( step_pin_m3, led_mode);   
+        gpio_set_direction( step_pin_m4, led_mode);   
+        gpio_set_direction( step_pin_m5, led_mode);   
+        gpio_set_direction( step_pin_m6, led_mode);   
+        gpio_set_direction( step_pin_m7, led_mode);   
+        gpio_set_direction( step_pin_m8, led_mode);    
+    
+    while (1){ 
+  gpio_set_level( step_pin_m1 ,1);  
+    
+  gpio_set_level( step_pin_m2, 1); 
+    
+  gpio_set_level( step_pin_m3 , 1); 
+    
+  gpio_set_level( step_pin_m4,1);  
+  
+  gpio_set_level( step_pin_m5, 1);  
+  
+  gpio_set_level( step_pin_m6, 1);  
+    
+  gpio_set_level( step_pin_m7 , 1); 
+    
+  gpio_set_level(step_pin_m8 ,1);    
+  
+  
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    
+   gpio_set_level( step_pin_m1 ,0);  
+   gpio_set_level( step_pin_m2 ,0);  
+   gpio_set_level( step_pin_m3 ,0);  
+   gpio_set_level( step_pin_m4 ,0);  
+   gpio_set_level( step_pin_m5 ,0);  
+   gpio_set_level( step_pin_m6 ,0);  
+   gpio_set_level( step_pin_m7,0);  
+   gpio_set_level( step_pin_m8 ,0);  
+            
+           
+     vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    
+     
+    }   
+    
+ 
+  
+  } 
