70669cd788354329e88d2182a560905d33ba0e46
proatharv <129577519+proatharv@users.noreply.github.com>
2023-09-09 16:11:27 +0530

    Update 1_dynamixel.c

--- Firmware/1_dynamixel.c
+++ Firmware/1_dynamixel.c
@@ -6,7 +6,7 @@
 #include "driver/gpio.h"  
 
 
-#define DYNAMIXEL_UART_NUM    UART_NUM_2
+
 #define BAUD_RATE               9600
 #define DYNAMIXEL_TX_PIN        17
 #define DYNAMIXEL_RX_PIN        16    
@@ -14,7 +14,7 @@
 
 
 void app_main(){ 
-    const uart_port_t uart_num = UART_NUM_2;
+     const uart_port_t DYNAMIXEL_UART_NUM = UART_NUM_2;
 uart_config_t dynamixel_config = {
         .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
@@ -22,20 +22,17 @@ uart_config_t dynamixel_config = {
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
         
-    };  
-
+    };
+    
     uart_param_config(DYNAMIXEL_UART_NUM, &dynamixel_config);
-    uart_set_pin(DYNAMIXEL_UART_NUM, DYNAMIXEL_TX_PIN, DYNAMIXEL_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    uart_driver_install(DYNAMIXEL_UART_NUM, 1024, 0, 0, NULL, 0); 
-    ESP_ERROR_CHECK(uart_set_mode(uart_num, UART_MODE_RS485_HALF_DUPLEX)); 
+    uart_set_pin(DYNAMIXEL_UART_NUM, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(DYNAMIXEL_UART_NUM, 1024, 0, 0, NULL, 0);
 
+     
 
-    char* test_str = "rotate by 30 ";
-    uart_write_bytes(uart_num, (const char*)test_str, strlen(test_str),DYNAMIXEL_ID); 
- 
-    uint8_t data[128];
-    int length ; 
-    length = uart_read_bytes(uart_num, data, length, DYNAMIXEL_ID, 100);  
-
+    uint8_t send_command( 0XFF  0XFF 1 0X05 0X03 0X08  0X1E  00);  
+    uart_write_bytes(DYNAMIXEL_TX_PIN, send_command ); 
+    uint8_t recieve_commmand( 0XFF  0XFF 1  05  02 00); 
+    uart_read_bytes(DYNAMIXEL_RX_PIN,recieve_commmand); 
       
 }
