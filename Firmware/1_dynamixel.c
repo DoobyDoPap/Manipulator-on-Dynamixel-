1369f00b9d637655ef40bc0333ad8f0724920e1b
proatharv <129577519+proatharv@users.noreply.github.com>
2023-09-09 17:49:42 +0530

    Update 1_dynamixel.c

--- Firmware/1_dynamixel.c
+++ Firmware/1_dynamixel.c
@@ -25,14 +25,15 @@ uart_config_t dynamixel_config = {
     };
     
     uart_param_config(DYNAMIXEL_UART_NUM, &dynamixel_config);
-    uart_set_pin(DYNAMIXEL_UART_NUM, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_set_pin(DYNAMIXEL_UART_NUM, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE); 
+    ESP_ERROR_CHECK(uart_set_mode(DYNAMIXEL_UART_NUM, UART_MODE_RS485_HALF_DUPLEX));
     uart_driver_install(DYNAMIXEL_UART_NUM, 1024, 0, 0, NULL, 0);
 
      
-
-    uint8_t send_command( 0XFF  0XFF 1 0X05 0X03 0X08  0X1E  00);  
-    uart_write_bytes(DYNAMIXEL_TX_PIN, send_command ); 
-    uint8_t recieve_commmand( 0XFF  0XFF 1  05  02 00); 
-    uart_read_bytes(DYNAMIXEL_RX_PIN,recieve_commmand); 
+    uint8_t uart_write_bytes(17, const float *send_command,sizeof(send_command),100); 
+    uint8_t send_command( 0XFF 0XFF  0X01  0X05 0X03 0X08  0X1E  0x00);  
+    
+    uint8_t recieve_commmand( 0XFF 0XFF 0X01  0X05  0X02 0X00); 
+    uint8_t uart_read_bytes(16, const float *recieve_commmand,sizeof(recieve_command),100); 
       
 }
