56d49eb719217e38bfb20cdf0b7fc1efc3b915f8
Isha <142431248+IshaNair09@users.noreply.github.com>
2023-09-10 19:59:17 +0530

    Update and rename setup.py to setup.py

--- ROS_Practice Folder/1_pubsub/setup.py
+++ ROS_Practice Folder/1_pubsub/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'py_pubsub'
+package_name = 'pubsub'
 
 setup(
     name=package_name,
@@ -13,17 +13,16 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-
     maintainer='isha',
     maintainer_email='isha@todo.todo',
-    description='Examples of minimal publisher/subscriber using rclpy',
-    license='Apache License 2.0',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-             'talker1 = py_pubsub.publisher_memberfunction:main',
-             'talker2 = py_pubsub.publisher_memberfunction2:main'
-             'listener = py_pubsub.subscriber_memberfunction:main',
+        'talker1 = pubsub.publisher_memberfunction:main',
+             'talker2 = pubsub.publisher_memberfunction2:main',
+             'listener = pubsub.subscriber_memberfunction:main',
         ],
     },
 )
